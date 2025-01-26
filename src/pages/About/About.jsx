11e33b8d71d2518644e0b7c4e23f8620e303b7da
vishallmaurya <vishallmaurya210@gmail.com>
2025-01-26 17:23:24 +0530

    skills added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,16 @@ export const About = () => {
         <>
             <div className={styles["introduction"]}>
                 <h2>About Me</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum sed, earum modi dolorem assumenda aut, repellat, eveniet commodi ratione soluta. Animi impedit quibusdam cum repellendus accusantium, quia dolores repellat? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus repudiandae a asperiores praesentium rem hic debitis sapiente ut doloremque delectus non, nobis suscipit voluptas sed nulla, rerum quia iure! Praesentium!</p>
+                <p>Hi, I'm Vishal Maurya, a software developer and data science enthusiast currently pursuing my MCA at NIT Jamshedpur. With a solid academic foundation and hands-on experience, I specialize in Python, Java, and C++ and have a strong interest in machine learning, data science, and web development.</p>
+                <p>
+                I'm the creator of veda-lib, a Python library for automating data preprocessing, published on PyPI to streamline workflows for data scientists. I've also worked on impactful projects like a sentiment analysis model with 89% accuracy, a Java-based quiz application.
+                </p>
+                <p>
+                As a researcher, I co-authored DWAEF: Deep Weighted Average Ensemble Framework, applying advanced ML techniques in NLP to detect sarcasm, published in a data science journal.
+                </p>
+                <p>
+                I'm also active in competitive programming, solving 500+ problems on LeetCode and contributing to technical teams and coding events at NIT Jamshedpur. With a passion for innovation, I'm always eager to learn, create, and collaborate on meaningful projects.
+                </p>
             </div>
         </>
     )
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -4,8 +4,15 @@ import styles from "./Skills.module.css";
 export const Skills = () => {
     return (
         <div className={styles["skills"]}>
-            <h2>Skills</h2>
-            <SkillCard image_url="react.png">React</SkillCard>
+            <h2>Skills</h2>        
+            <div className={styles["skillscard"]}>
+                <SkillCard image_url="mongodb.png">Mongodb</SkillCard>
+                <SkillCard image_url="express.png">Express</SkillCard>
+                <SkillCard image_url="react.png">React</SkillCard>
+                <SkillCard image_url="nodejs.png">Nodejs</SkillCard>
+                <SkillCard image_url="ml.png">Machine Learning</SkillCard>
+                <SkillCard image_url="dl.png">Deep Learning</SkillCard>
+            </div>
         </div>
     )
 }
