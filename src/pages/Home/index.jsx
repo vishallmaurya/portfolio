dc3bd0538491f0680f35d6e389e48eb4e990d391
vishallmaurya <vishallmaurya210@gmail.com>
2025-01-26 18:15:07 +0530

    rectify css errors

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,39 @@
 import { Logo } from "../../components/Logo";
 import { About } from "../About";
-import { Skills } from "../Skills/Skills";
+import { Skills } from "../Skills";
 import { Button } from "../../components/Button";
 
 import styles from "./Home.module.css";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { Works } from "../Works";
 
 
 export const Home = () => {
     return (
         <div className={styles["header-container"]}>
             <div className={styles["main-content-container"]}>
-                <div className={styles["image-container"]}>
-                    <img src="logo.png" alt="" />
+                <div className={ styles["center"] }>
+                    <div className={styles["image-container"]}>
+                        <img src="logo.png" alt="" />
+                    </div>
                 </div>
-                <span>VISHAL MAURYA</span>
+                <div className={styles["center"]}>
+                    <span>VISHAL MAURYA</span>
+                </div>
                 <Logo />
-                <Button logo={faArrowDown} classname={styles["button-resume"]}>Resume</Button>
+                <div className={styles["center"]}>
+                    <Button logo={faArrowDown} classname={styles["button-resume"]}>Resume</Button>
+                </div>
                 <div className={styles["introduction-container"]}>
                     <About />
                     <Skills />
+                </div>
+                
+                <div className={styles["works-heading"]}>
+                    My Skills
+                </div>
+                <div className={styles["works-container"]}>
+                    <Works />
                 </div>
             </div>
         </div>
---
+++ src/pages/Works/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./Works.module.css";
+
+export const Works = () => {
+    return (
+        <div className={ styles["works-container"] }>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui officia et repellendus alias, provident illo deleniti magnam! Labore nobis ducimus vel. Maxime eligendi debitis eum? Totam consequatur saepe ducimus tenetur.
+            Accusamus neque assumenda velit tenetur nostrum, architecto ea quis eum. Adipisci at optio tenetur fuga qui mollitia vel? Accusamus nam doloremque soluta autem esse atque velit officiis explicabo a illo.
+            Pariatur omnis blanditiis modi doloribus, placeat sit maiores velit? Quidem totam quis repellat cupiditate accusantium deleniti voluptate veritatis animi iste magni nisi quas corporis laborum dolorem, dolorum repudiandae autem neque.
+            Saepe consectetur mollitia labore inventore aliquam quidem fugit dolorum libero ex ducimus, delectus blanditiis dignissimos aut dolores veniam voluptatem voluptatum nobis! Sequi nesciunt magni odit? Similique nesciunt minima ullam doloribus.
+            Provident, earum praesentium. Odio placeat est accusamus fuga repellat aperiam quam consequuntur cupiditate voluptatum tempore ipsum facere laudantium molestiae magnam perferendis vero aspernatur hic recusandae, tempora aliquam illum ducimus. Saepe!
+        </div>
+    )
+}
