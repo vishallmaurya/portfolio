c9f32694f9aa21b5381332ed832a335c2678c82c
vishallmaurya <vishallmaurya210@gmail.com>
2025-01-26 23:03:46 +0530

    images uploaded

---
+++ src/components/WorkCard.jsx
@@ -0,0 +1,9 @@
+import styles from "./css/workcard.module.css";
+
+export const WorkCard = () => {
+    return (
+        <div className={styles["workcard"]}>
+            
+        </div>
+    )
+}
--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -1,13 +1,15 @@
+import { WorkCard } from "../../components/WorkCard";
 import styles from "./Works.module.css";
 
 export const Works = () => {
     return (
-        <div className={ styles["works-container"] }>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui officia et repellendus alias, provident illo deleniti magnam! Labore nobis ducimus vel. Maxime eligendi debitis eum? Totam consequatur saepe ducimus tenetur.
-            Accusamus neque assumenda velit tenetur nostrum, architecto ea quis eum. Adipisci at optio tenetur fuga qui mollitia vel? Accusamus nam doloremque soluta autem esse atque velit officiis explicabo a illo.
-            Pariatur omnis blanditiis modi doloribus, placeat sit maiores velit? Quidem totam quis repellat cupiditate accusantium deleniti voluptate veritatis animi iste magni nisi quas corporis laborum dolorem, dolorum repudiandae autem neque.
-            Saepe consectetur mollitia labore inventore aliquam quidem fugit dolorum libero ex ducimus, delectus blanditiis dignissimos aut dolores veniam voluptatem voluptatum nobis! Sequi nesciunt magni odit? Similique nesciunt minima ullam doloribus.
-            Provident, earum praesentium. Odio placeat est accusamus fuga repellat aperiam quam consequuntur cupiditate voluptatum tempore ipsum facere laudantium molestiae magnam perferendis vero aspernatur hic recusandae, tempora aliquam illum ducimus. Saepe!
+        <div>
+            <WorkCard img_url="dwaef.png">content</WorkCard>
+            <WorkCard img_url="veda.png">content</WorkCard>
+            <WorkCard img_url="calmspace.png">content</WorkCard>
+            <WorkCard img_url="sa.jpeg">content</WorkCard>
+            <WorkCard img_url="java.png">content</WorkCard>
+            <WorkCard img_url="cf.jpeg">content</WorkCard>
         </div>
     )
 }
