07f8910afe69188c96b022cad361d9fd8868406c
vishallmaurya <vishallmaurya210@gmail.com>
2025-01-27 17:16:02 +0530

    work section added

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,4 +1,7 @@
 import styles from "./css/workcard.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub} from "@fortawesome/free-brands-svg-icons";
+
 
 export const WorkCard = ({
     img_url = "",
@@ -9,14 +12,25 @@ export const WorkCard = ({
     return (
         <div className={styles["workcard"]} {...props}>
             <div className={styles["image-container"]}>
-                <img src={img_url} />
+                <img src={img_url} className={ styles["image-container-img"] } />
                 <div className={styles["content-overlay"]}>
                     <p>{overlay_content}</p>
                     <div className={ styles["content-overlay-btns"] }>
-                        <div className="content-overlay-btn">
-                            <div className="btn-image">
-
-                            </div>        
+                        <div className={styles["content-overlay-btn"]}>
+                            <div className={styles["btn-image"]}>
+                                <img src="demo.png" alt="" />
+                            </div>
+                            <div className={styles["btn-content"]}>
+                                Demo
+                            </div>
+                        </div>
+                        <div className={styles["content-overlay-btn"]}>
+                            <div className={styles["btn-image"]}>
+                                <FontAwesomeIcon icon={faGithub} size="2x"/>
+                            </div>
+                            <div className={styles["btn-content"]}>
+                                Code
+                            </div>
                         </div>
                     </div>
                 </div>
